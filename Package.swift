e5b48c099c191611faf719ab3b54befe358e0b80
Adrien Cantérot <adrien.canterot@gmail.com>
2016-07-14 12:38:39 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporSQLite",
     dependencies: [
-   		.Package(url: "https://github.com/qutheory/fluent-sqlite.git", majorVersion: 0, minor: 6),
+   		.Package(url: "https://github.com/qutheory/fluent-sqlite.git", majorVersion: 0, minor: 7),
    		.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
     ]
 )
