740012401da0583512dd3d8d7566a48dc7957c80
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-08 01:14:16 +0200

    testProviderWithPathInit implemented

--- Tests/VaporSQLite/SQLiteTests.swift
+++ Tests/VaporSQLite/SQLiteTests.swift
@@ -1,13 +1,33 @@
 import XCTest
 @testable import VaporSQLite
 import Fluent
+import Vapor
 
 class SQLiteTests: XCTestCase {
     static let allTests = [
         ("testReality", testReality)
     ]
-
-    func testReality() {
-        XCTAssert(2 + 2 == 4, "Something is seriously wrong.")
+    
+    override func setUp() {
+        do {
+            try Provider.createTestTable()
+        } catch {
+            XCTFail("Could not set up : \(error)")
+        }
+        
+    }
+    
+    func testProviderWithPathInit throws {
+        
+        let sqliteProvider = try Provider(path: "database.db")
+        
+        let droplet = Droplet(initializedProviders: [sqliteProvider])
+        
+        var user = User(name: "Jordy")
+        User.database = droplet.database
+        try user.save()
+        
+        let fetched = try User.find(1)
+        XCTAssertEqual(user.name, fetched?.name)
     }
 }
---
+++ Tests/VaporSQLite/Utilities/User.swift
@@ -0,0 +1,33 @@
+import Vapor
+import Fluent
+
+final class User: Model {
+    var id: Node?
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        name = try node.extract("name")
+    }
+
+    func makeNode() throws -> Node {
+        return try Node(node: [
+            "name": name
+        ])
+    }
+
+    static func prepare(_ database: Database) throws {
+        try database.create(entity) { builder in
+            builder.id()
+            builder.string("name")
+        }
+    }
+
+    static func revert(_ database: Database) throws {
+        //
+    }
+}
