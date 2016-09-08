57359902a78713afc0f59a534102e3b02cc04aae
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-08 17:57:24 +0200

    Set Package.swift to vapor dependencies only

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporSQLite",
     dependencies: [
-   		.Package(url: "https://github.com/adriencanterot/sqlite-driver.git", majorVersion: 0, minor: 6),
+   		.Package(url: "https://github.com/vapor/sqlite-driver.git", majorVersion: 0, minor: 6),
    		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17),
     ]
 )
