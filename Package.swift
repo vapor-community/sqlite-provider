1ff916936de550e05225c2e6c49e72454d738291
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-09 08:19:38 +0200

    GM Support

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporSQLite",
     dependencies: [
    		.Package(url: "https://github.com/adriencanterot/sqlite-driver.git", majorVersion: 0, minor: 6),
-   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17),
+   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18),
     ]
 )
